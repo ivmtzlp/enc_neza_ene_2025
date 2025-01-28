c559e7e117aaa4636565432c88f51bd390b2ae3b
CAMS3D <cypriemere@gmail.com>
2025-01-28 13:34:27 -0600

    entregable del bloque de conocimiento de elecciones judiciales

---
+++ R/entregable/entregable_bloque_conocimiento_elecciones_judiciales.R
@@ -0,0 +1,38 @@
+library(officer)
+library(dplyr)
+
+path_export <- 
+  encuestar:::formato_archivo(nombre = "./presentaciones/entregable_bloque_conocimiento_elecciones_judiciales", 
+                              extension = "pptx", 
+                              tolerancia = 60)
+
+pptx <- 
+  read_pptx(path = "./insumos/plantilla_general_09_12_24.pptx")
+
+
+add_slide(pptx, layout = "gerencia_subportada", master = "gerencia") %>%
+  ph_with(value = 'Elecciones judiciales',
+          location = ph_location_label(ph_label = "titulo")) 
+
+
+# Conocimeinto de fecha de eleccion
+
+
+add_slide(pptx, layout = "gerencia_grafica_unica", master = "gerencia") %>%
+  ph_with(value = p_eleccion_pjn_fecha_graf, 
+          location = ph_location_label(ph_label = "imagen_principal")) |>
+  ph_with(value = "Fechas de las elecciones judiciales",
+          location = ph_location_label(ph_label = "titulo"))
+
+
+# Intencion de participacion a eleccion
+
+add_slide(pptx, layout = "gerencia_grafica_unica", master = "gerencia") %>%
+  ph_with(value = p_eleccion_pjn_participacion_graf , 
+          location = ph_location_label(ph_label = "imagen_principal")) |>
+  ph_with(value = "Intencion elecotral",
+          location = ph_location_label(ph_label = "titulo"))
+
+
+
+print(pptx, path_export)
