61423b91faeb2bdaa9c5c5c5079775ac66c05c26
CAMS3D <cypriemere@gmail.com>
2025-01-28 12:48:30 -0600

    entregable bloque desepeño gubernamental

---
+++ R/entregable/entregable_bloque_desempeno_gubernamental.R
@@ -0,0 +1,55 @@
+library(officer)
+library(dplyr)
+
+path_export <- 
+  encuestar:::formato_archivo(nombre = "./presentaciones/bloque_desempeno_gubernamental", 
+                              extension = "pptx", 
+                              tolerancia = 60)
+
+pptx <- 
+  read_pptx(path = "./insumos/plantilla_general_09_12_24.pptx")
+
+
+add_slide(pptx, layout = "gerencia_subportada", master = "gerencia") %>%
+  ph_with(value = 'Conocimiento Personajes Secundarios',
+          location = ph_location_label(ph_label = "titulo")) 
+
+# Desempeno gobierno delfina
+
+ 
+add_slide(pptx, layout = "gerencia_grafica_unica", master = "gerencia") %>%
+  ph_with(value = p_delfina_1_graf, 
+          location = ph_location_label(ph_label = "imagen_principal")) |>
+  ph_with(value = "Evaluación ciudadana de Delfina Gómez",
+          location = ph_location_label(ph_label = "titulo"))
+
+
+# Satisfación sevicios 
+
+add_slide(pptx, layout = "gerencia_grafica_unica", master = "gerencia") %>%
+  ph_with(value = g_satisfacion, 
+          location = ph_location_label(ph_label = "imagen_principal")) |>
+  ph_with(value = "Evaluación de servicios publicos",
+          location = ph_location_label(ph_label = "titulo"))
+
+# Problemas agua
+
+
+add_slide(pptx, layout = "gerencia_grafica_unica", master = "gerencia") %>%
+  ph_with(value = p_problemas_agua_graf , 
+          location = ph_location_label(ph_label = "imagen_principal")) |>
+  ph_with(value = "Frecuencia de problemas con el suministro de agua",
+          location = ph_location_label(ph_label = "titulo"))
+
+# Mejora inmediata
+
+add_slide(pptx, layout = "gerencia_grafica_unica", master = "gerencia") %>%
+  ph_with(value = p_mejora_servicio_graf , 
+          location = ph_location_label(ph_label = "imagen_principal")) |>
+  ph_with(value = "Mejoras inmediatas solicitadas ",
+          location = ph_location_label(ph_label = "titulo"))
+
+
+
+
+print(pptx, path_export)
