35550151d513d17ffffb5d5d5aea1387fbe842ae
CAMS3D <cypriemere@gmail.com>
2025-02-04 13:03:18 -0600

    cambios a las graficas

--- R/cruces/cruces_bloque_gobiernos.R
+++ R/cruces/cruces_bloque_gobiernos.R
@@ -11,20 +11,21 @@ enc_neza$Resultados$diseno$variables %>%
   glimpse()
 p_conoce_per_cruce_sexo_graf <-
   
-  enc_neza$Resultados$Cruce$lolipop_diferencias(
+  enc_neza$Resultados$Cruce$lolipop_diferencias( 
         variable_principal = "sexo_cat",
-        variables_secundarias = c("conoce_per_delfina", "conoce_per_claudia", "conoce_per_neza"),
+        variables_secundarias = c("conoce_per_neza","conoce_per_delfina", "conoce_per_claudia"),
         filtro_variables_secundarias = "Sí lo conoce", 
-        orden_variablePrincipal = c("Delfina Gómez Álvarez", "Claudia Sheinbaum", "Adolfo Cerqueda Rebollo"),
-        colores_variables_secundarias = c("Hombre" = "blue", "Mujer" = "pink"),  # Mantén los colores con las etiquetas originales
-        caption = "Conocimiento de personaje por sexo", 
+        orden_variablePrincipal = c( "Adolfo Cerqueda Rebollo","Delfina Gómez Álvarez","Claudia Sheinbaum" ),
+        colores_variables_secundarias = c("Hombre" = "#A6032F", "Mujer" = "#F5CD5F"),  
+        caption = "Conocimiento de autoridades por sexo", 
         invertir_variables = TRUE,
         limits = c(0,1.05), 
-        nudge_x = 0.15,
-        wrap_y = 1,
-        wrap_caption = 50,
-        traslape = 0.18,
+        nudge_x = 0.3,
+        wrap_y = 80,
+        wrap_ = 90,
+        traslape = T,
         limite_dif_pct = 0.02,
+        ajuste_pos = 0.03
         
        )
    
@@ -55,12 +56,12 @@ p_conoce_per_cruce_edad_graf <-
                                                   
                                                    caption = "Conocimiento de personaje por edad", 
                                                    invertir_variables = F,
-                                                   limits = c(0,1.09), 
-                                                   nudge_x= 0.15,
+                                                   limits = c(0,1.1), 
+                                                   nudge_x= 0.3,
                                                    wrap_y = 60,
-                                                   wrap_caption = 60,
-                                                   traslape =0.18,
-                                                   limite_dif_pct = 0.03
+                                                   wrap_caption = 20, size_text_legend = 15,
+                                                   traslape =T,
+                                                   limite_dif_pct = 0.5
                                                    
                                                    
     )
